cf9d6e5f0b3c144a72a8b10ecd84b2c8bfe6fa36
Osmond Jiang <xinjiang@microsoft.com>
2024-08-09 17:38:03 +0800

    use msi

--- raft/client_utils.py
+++ raft/client_utils.py
@@ -5,6 +5,9 @@ from openai import AzureOpenAI, OpenAI
 import logging
 from env_config import read_env_config, set_env
 from os import environ
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from openai import AzureOpenAI
+
 
 logger = logging.getLogger("client_utils")
 
@@ -14,11 +17,11 @@ def build_openai_client(**kwargs: Any) -> OpenAI:
     """
     Build OpenAI client based on the environment variables.
     """
-
+    token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")  
     env = read_env_config("COMPLETION")
     with set_env(**env):
         if is_azure():
-            client = AzureOpenAI(**kwargs)
+            client = AzureOpenAI(api_version="2024-02-15-preview", azure_endpoint="https://learn-shared-eastus.openai.azure.com/", azure_ad_token_provider=token_provider)
         else:
             client = OpenAI(**kwargs)
         return client
@@ -28,19 +31,21 @@ def build_langchain_embeddings(**kwargs: Any) -> OpenAIEmbeddings:
     Build OpenAI embeddings client based on the environment variables.
     """
 
+    token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
     env = read_env_config("EMBEDDING")
 
     with set_env(**env):
         if is_azure():
-            client = AzureOpenAIEmbeddings(**kwargs)
+            client = AzureOpenAIEmbeddings(azure_endpoint="https://learn-shared-eastus.openai.azure.com/", azure_ad_token_provider=token_provider)
         else:
             client = OpenAIEmbeddings(**kwargs)
         return client
 
 def is_azure():
-    azure = "AZURE_OPENAI_ENDPOINT" in environ or "AZURE_OPENAI_KEY" in environ or "AZURE_OPENAI_AD_TOKEN" in environ
-    if azure:
-        logger.debug("Using Azure OpenAI environment variables")
-    else:
-        logger.debug("Using OpenAI environment variables")
-    return azure
+    # azure = "AZURE_OPENAI_ENDPOINT" in environ or "AZURE_OPENAI_KEY" in environ or "AZURE_OPENAI_AD_TOKEN" in environ
+    # if azure:
+    #     logger.info("Using Azure OpenAI environment variables")
+    # else:
+    #     logger.info("Using OpenAI environment variables")
+    # return azure
+    return True
